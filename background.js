d58eaa47fd182a43547da2207a87bed6acf8c6e0
Imrane <imranem3@hotmail.com>
2024-11-27 03:08:03 -0700

    comment clarifications

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-//for when the extension icon is clicked, a new note will spawn in the middle of the screen
+//For when the extension icon is clicked; a new note will spawn in the middle of the screen
 chrome.action.onClicked.addListener((tab) => {
     chrome.tabs.sendMessage(tab.id, { action: "createNote" });
   });
